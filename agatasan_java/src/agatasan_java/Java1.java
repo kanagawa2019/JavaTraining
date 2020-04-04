8a5da2ab9e1fbed1bb1bee3225d393ad981f0d3d
developlocal <developlocalhm2019@gmail.com>
2020-04-04 19:30:09 +0900

    Java1_1の内容取り込み

--- agatasan_java/src/agatasan_java/Java1.java
+++ agatasan_java/src/agatasan_java/Java1.java
@@ -2,14 +2,36 @@ package agatasan_java;
 
 import java.util.stream.IntStream;
 
+/**
+ * Java1クラス 九九表示クラス
+ *
+ * @author 菱田 美紀
+ * @version 1.0 2020/03/04 新規作成
+ * @version 1.1 2020/03/05 2020/03/05のRV指摘取り込み　マジックナンバーを定数化、表示を整形
+ * @version 1.2 2020/03/07 2020/03/06のRV指摘取り込み
+ * @version 1.3 2020/03/11 2020/03/10のRV指摘取り込み
+ * @version 1.4 2020/04/04 2020/03/24のRV指摘取り込み　Git管理化、ファイルを1つに集約
+ */
 public class Java1 {
 
+    /** 九九の始まりの値 */
+    private static final int MULTIPLICATION_TABLE_RANGE_START = 1;
+
+    /** 九九の終わりの値 */
+    private static final int MULTIPLICATION_TABLE_RANGE_END = 9;
+
+    /**
+     * 九九の計算を行い、コンソールに表示します。
+     * 
+     */
     public static void main(String[] args) {
-        IntStream.rangeClosed(1, 9).forEach(i -> {
-            IntStream.rangeClosed(1, 9).forEach(j -> System.out.print(i * j + " "));
-            System.out.println("");
+        IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(i -> {
+            IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(j -> {
+                System.out.printf("%2d", i * j);
+                System.out.print(" ");
+            });
+            System.out.printf("%n");
         });
 
     }
-
 }
