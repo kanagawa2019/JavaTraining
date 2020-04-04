b401bf04e06a87a7ab4857ecce431f5b2881e5c3
developlocal <developlocalhm2019@gmail.com>
2020-04-04 19:37:35 +0900

    Java1_3の内容取り込み

--- agatasan_java/src/agatasan_java/Java1.java
+++ agatasan_java/src/agatasan_java/Java1.java
@@ -9,7 +9,7 @@ import java.util.stream.IntStream;
  * @version 1.0 2020/03/04 新規作成
  * @version 1.1 2020/03/05 2020/03/05のRV指摘取り込み　マジックナンバーを定数化、表示を整形
  * @version 1.2 2020/03/07 2020/03/06のRV指摘取り込み　javaDocの記載追加
- * @version 1.3 2020/03/11 2020/03/10のRV指摘取り込み
+ * @version 1.3 2020/03/11 2020/03/10のRV指摘取り込み　表示形式を定数化
  * @version 1.4 2020/04/04 2020/03/24のRV指摘取り込み　Git管理化、ファイルを1つに集約
  */
 public class Java1 {
@@ -19,6 +19,9 @@ public class Java1 {
 
     /** 九九の終わりの値 */
     private static final int MULTIPLICATION_TABLE_RANGE_END = 9;
+    
+    /** 表示形式 */
+    private static final String DISPLAY_FORMAT = "%2d%s";
 
     /**
      * 九九の計算を行い、コンソールに表示します。
@@ -27,8 +30,7 @@ public class Java1 {
     public static void main(String[] args) {
         IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(i -> {
             IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(j -> {
-                System.out.printf("%2d", i * j);
-                System.out.print(" ");
+                System.out.printf(DISPLAY_FORMAT, i * j, " ");
             });
             System.out.printf("%n");
         });
