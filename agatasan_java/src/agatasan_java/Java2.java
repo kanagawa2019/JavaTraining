e3e677781dba8f28d7dd7f5497a0b374b41c35a8
developlocal <developlocalhm2019@gmail.com>
2020-04-26 16:44:48 +0900

    longで入力できる範囲を作成したが、long桁数を超えるとcatchにいくので、intでもよい気がする

--- agatasan_java/src/agatasan_java/Java2.java
+++ agatasan_java/src/agatasan_java/Java2.java
@@ -9,7 +9,7 @@ import java.util.stream.IntStream;
  *
  * @author 菱田 美紀
  * @version 1.0 2020/03/08 新規作成
- * @version 1.1 2020/04/19 2020/03/24のRV指摘取り込み　無限ループwhile(true)を変更
+ * @version 1.1 2020/04/19 2020/03/24のRV指摘取り込み 無限ループwhile(true)を変更
  */
 public class Java2 {
 
@@ -21,48 +21,67 @@ public class Java2 {
 
     /** 表示形式 */
     private static final String DISPLAY_FORMAT = "%2s ";
-    
+
     /** 終了条件 */
     private static final String EXIT_CONDITIONS = "end";
-    
+
     /**
      * 入力された数値で九九の計算を行い、コンソールに表示します。
      * 
      */
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("数値を入力してください。");
         System.out.println("endで入力を終了します。");
-        
-            while(scanner.hasNext()) {
-                
-                // 値取得
-                String input = scanner.next();
-                
-                // endか判定
-                if (Objects.equals(input, EXIT_CONDITIONS)) {
-                    // 終了
-                    break;
-                }
-                
-                // 数値か判定
-                try {
-                    int line = Integer.valueOf(input).intValue();
-                    // 入力内容が数値の場合
-                    IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(i -> {
-                        System.out.printf(DISPLAY_FORMAT, i * line, " ");
-                    });
-                } catch (NumberFormatException e) {
-                    // 入力内容が数値以外の場合
-                    System.out.println("エラー！！数値以外が入力されました。");
-                    System.out.println("数値を入力してください。");
-                }
-                
+
+        while (scanner.hasNext()) {
+
+            // 値取得
+            String input = scanner.next();
+
+            // endか判定
+            if (Objects.equals(input, EXIT_CONDITIONS)) {
+                // 終了
+                break;
             }
-            
-            // ctr + z or "end"入力で終了
-            scanner.close();
-            System.out.println("入力を終了しました。");
+
+            // 数値か判定
+            try {
+//                int line = Integer.valueOf(input).intValue();
+                long line = Long.parseLong(input);
+
+                // 入力範囲が1~9以外はエラー
+                if (!isRange(line, 1, 9)) {
+                    System.out.println("エラー！！1～9以外の数値が入力されました。");
+                    System.out.println("1～9の数値を入力してください。");
+                    continue;
+                }
+
+                // 入力内容が数値の場合
+                IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(i -> {
+                    System.out.printf(DISPLAY_FORMAT, i * line, " ");
+                });
+//                System.out.println("");
+                System.out.println("\r\n引き続き、九九表示する場合は、数値を入力してください。");
+            } catch (NumberFormatException e) {
+                // 入力内容が数値以外の場合
+                System.out.println("エラー！！数値以外が入力されました。");
+                System.out.println("1～9の数値を入力してください。");
+            }
+
+        }
+
+        // ctr + z or "end"入力で終了
+        scanner.close();
+        System.out.println("入力を終了しました。");
+    }
+
+//    public static boolean isRange(int x, int from, int to) {
+//        return (x >= from && x <= to);
+//    }
+
+    public static boolean isRange(long x, int from, int to) {
+        return (x >= (long) from && x <= (long) to);
     }
 }
