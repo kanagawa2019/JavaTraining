344784ad5815e4a14b3759890c3a497f3e639308
developlocal <developlocalhm2019@gmail.com>
2020-04-27 23:11:48 +0900

    version 1.3 作成

--- agatasan_java/src/agatasan_java/Java2.java
+++ agatasan_java/src/agatasan_java/Java2.java
@@ -12,6 +12,7 @@ import java.util.stream.IntStream;
  * @version 1.1 2020/04/19 2020/03/24のRV指摘取り込み 無限ループwhile(true)を変更
  * @version 1.2 2020/04/26 2020/04/20のRV指摘取り込み 入力上限チェックの追加・whileインデント修正
  *          ・入力完了（九九表示）後のユーザー入力促し文言の追加
+ * @version 1.3 2020/04/27 2020/04/27のRV指摘取り込み 文言定数化・メソッド命名規則化・不等号の向きを揃える
  */
 public class Java2 {
 
@@ -27,6 +28,15 @@ public class Java2 {
     /** 終了条件 */
     private static final String EXIT_CONDITIONS = "end";
 
+    /** ユーザー入力促し文言 */
+    private static final String USER_INPUT_SENTENCES = "数値を入力してください。";
+
+    /** 数値入力促し文言 */
+    private static final String NUMBER_INPUT_SENTENCES = "1～9の数値を入力してください。";
+
+    /** 数値以外の入力時エラー文言 */
+    private static final String NON_NUMERIC_ERROR = "エラー！！1～9以外の数値が入力されました。";
+
     /**
      * 入力された数値で九九の計算を行い、コンソールに表示します。
      * 
@@ -34,7 +44,7 @@ public class Java2 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("数値を入力してください。");
+        System.out.println(USER_INPUT_SENTENCES);
         System.out.println("endで入力を終了します。");
 
         while (scanner.hasNext()) {
@@ -53,9 +63,9 @@ public class Java2 {
                 int line = Integer.valueOf(input).intValue();
 
                 // 入力範囲が1~9以外はエラー
-                if (!isRange(line, 1, 9)) {
-                    System.out.println("エラー！！1～9以外の数値が入力されました。");
-                    System.out.println("1～9の数値を入力してください。");
+                if (!isWithinRange(line, 1, 9)) {
+                    System.out.println(NON_NUMERIC_ERROR);
+                    System.out.println(NUMBER_INPUT_SENTENCES);
                     continue;
                 }
 
@@ -66,8 +76,8 @@ public class Java2 {
                 System.out.printf("%n引き続き、九九表示する場合は、数値を入力してください。%n");
             } catch (NumberFormatException e) {
                 // 入力内容が数値以外の場合
-                System.out.println("エラー！！数値以外が入力されました。");
-                System.out.println("1～9の数値を入力してください。");
+                System.out.println(NON_NUMERIC_ERROR);
+                System.out.println(NUMBER_INPUT_SENTENCES);
             }
 
         }
@@ -80,13 +90,13 @@ public class Java2 {
     /**
      * 数値範囲チェック
      * 
-     * @param x    対象数値
-     * @param from 範囲FROM
-     * @param to   範囲TO
-     * @return true:範囲FROM～範囲TO内である
+     * @param targetNumber チェック対象数値
+     * @param minValue     最小値
+     * @param maxValue     最大値
+     * @return true:最小値～最大値の範囲内にある
      */
-    public static boolean isRange(int x, int from, int to) {
-        return (x >= from && x <= to);
+    public static boolean isWithinRange(int targetNumber, int minValue, int maxValue) {
+        return (minValue <= targetNumber && targetNumber <= maxValue);
     }
 
 }
