dad2f2b3eff858b5efdfb578919e232d33395f60
developlocal <developlocalhm2019@gmail.com>
2020-05-03 19:06:33 +0900

    java4いろいろ変更前

--- agatasan_java/src/agatasan_java/Java4.java
+++ agatasan_java/src/agatasan_java/Java4.java
@@ -18,132 +18,155 @@ public class Java4 {
     private static final int MULTIPLICATION_TABLE_RANGE_END = 9;
 
     /** 表示形式 */
-    private static final String DISPLAY_FORMAT = "%2s%s";
-    
-    /** 3の倍数時の表示 */
-    private static final String MULTIPLES_OF_3_DISPLAY = "NA";
-    
+    private static final String DISPLAY_FORMAT = "%2s  ";
+
+    /** 倍数 */
+    private static final int MULTIPLE = 3;
+
+    /** s倍数時の表示 */
+    private static final String MULTIPLES_OF_DISPLAY = "NA";
+
+    /** ユーザー入力促し文言 */
+    private static final String USER_INPUT_SENTENCES = "数値を入力してください。";
+
+    /** 入力許容最小値 */
+    private static final int USER_INPUT_MIN_VALUE = 1;
+
+    /** 入力許容最大値 */
+    private static final int USER_INPUT_MAX_VALUE = 9;
+
+    /** 入力許容範囲 */
+    private static final String USER_INPUT_RANGE = USER_INPUT_MIN_VALUE + "～" + USER_INPUT_MAX_VALUE;
+
+    /** 数値以外の入力時エラー文言 */
+    private static final String NON_NUMERIC_ERROR = "エラー！！" + USER_INPUT_RANGE + "以外の数値が入力されました。";
+
     /**
      * 入力された数値で九九の計算を行い、コンソールに表示します。
      * 
      */
     public static void main(String[] args) {
-        
+
         Scanner scanner = new Scanner(System.in);
 
         // 切替モードオン・オフ
         boolean displaySwitching = false;
-        
-            while(true) {
-                
-                System.out.println("***********************************");
-                System.out.println("処理モードを選択してください");
-                System.out.println("1.九九表示");
-                System.out.println("2.指定の段のみ表示");
-                if (displaySwitching) {
-                    System.out.println("3.モード切替(世界のナベアツ⇒通常)");
-                } else {
-                    System.out.println("3.モード切替(通常⇒世界のナベアツ)");
+
+        while (true) {
+
+            System.out.println("***********************************");
+            System.out.println("処理モードを選択してください");
+            System.out.println("1.九九表示");
+            System.out.println("2.指定の段のみ表示");
+            if (displaySwitching) {
+                System.out.println("3.モード切替(世界のナベアツ⇒通常)");
+            } else {
+                System.out.println("3.モード切替(通常⇒世界のナベアツ)");
+            }
+
+            System.out.println("9.終了");
+            System.out.println("***********************************");
+
+            if (scanner.hasNextInt()) {
+                // 入力内容が数値の場合
+                int line = scanner.nextInt();
+
+                // 1が入力された場合
+                if (line == 1) {
+                    if (displaySwitching) {
+                        // 切替モードオンの場合
+                        IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END)
+                                .forEach(i -> {
+                                    IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START,
+                                            MULTIPLICATION_TABLE_RANGE_END).forEach(
+                                                    j -> {
+
+                                                        // 3の倍数は別表示
+                                                        if ((i * j) % MULTIPLE == 0) {
+                                                            System.out.printf(DISPLAY_FORMAT, MULTIPLES_OF_DISPLAY,
+                                                                    " ");
+                                                        } else {
+                                                            System.out.printf(DISPLAY_FORMAT, i * j, " ");
+                                                        }
+                                                    });
+                                    System.out.printf("%n");
+                                });
+                    } else {
+                        // 切替モードオフの場合
+                        IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END)
+                                .forEach(i -> {
+                                    IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START,
+                                            MULTIPLICATION_TABLE_RANGE_END).forEach(j -> {
+                                                System.out.printf(DISPLAY_FORMAT, i * j, " ");
+                                            });
+                                    System.out.printf("%n");
+                                });
+                    }
+
                 }
-                
-                System.out.println("9.終了");
-                System.out.println("***********************************"); 
-                
-                
-                if (scanner.hasNextInt()) {
-                    // 入力内容が数値の場合
-                    int line = scanner.nextInt();
-                    
-                    // 1が入力された場合
-                    if (line == 1) {
+
+                // 2が入力された場合
+                if (line == 2) {
+                    System.out.println("表示する段を入力してください");
+
+                    if (scanner.hasNextInt()) {
+                        // 入力内容が数値の場合
+                        int inputLine = scanner.nextInt();
+
                         if (displaySwitching) {
                             // 切替モードオンの場合
-                            IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(i -> {
-                                IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(j -> {
-                                    
-                                    // 3の倍数は別表示
-                                    if ((i * j) % 3 == 0) {
-                                        System.out.printf(DISPLAY_FORMAT, MULTIPLES_OF_3_DISPLAY, " ");
-                                    } else {
-                                        System.out.printf(DISPLAY_FORMAT, i * j, " ");
-                                    }
-                                });
-                                System.out.printf("%n");
-                            });
+                            IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END)
+                                    .forEach(i -> {
+
+                                        // 3の倍数は別表示
+                                        if ((i * inputLine) % 3 == 0) {
+                                            System.out.printf(DISPLAY_FORMAT, MULTIPLES_OF_DISPLAY, " ");
+                                        } else {
+                                            System.out.printf(DISPLAY_FORMAT, i * inputLine, " ");
+                                        }
+
+                                    });
+                            System.out.printf("%n");
                         } else {
                             // 切替モードオフの場合
-                            IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(i -> {
-                                IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(j -> {
-                                    System.out.printf(DISPLAY_FORMAT, i * j, " ");
-                                });
-                                System.out.printf("%n");
-                            });
-                        }
-
-                    }
-                    
-                    // 2が入力された場合
-                    if (line == 2) {
-                        System.out.println("表示する段を入力してください");
-                        
-                        if (scanner.hasNextInt()) {
-                            // 入力内容が数値の場合
-                            int inputLine = scanner.nextInt();
-
-                            
-                            if (displaySwitching) {
-                                // 切替モードオンの場合
-                                IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(i -> {
-                                    
-                                    // 3の倍数は別表示
-                                    if ((i * inputLine) % 3 == 0) {
-                                        System.out.printf(DISPLAY_FORMAT, MULTIPLES_OF_3_DISPLAY, " ");
-                                    } else {
+                            IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END)
+                                    .forEach(i -> {
                                         System.out.printf(DISPLAY_FORMAT, i * inputLine, " ");
-                                    }
-                                    
-                                });
-                                System.out.printf("%n");
-                            } else {
-                                // 切替モードオフの場合
-                                IntStream.rangeClosed(MULTIPLICATION_TABLE_RANGE_START, MULTIPLICATION_TABLE_RANGE_END).forEach(i -> {
-                                    System.out.printf(DISPLAY_FORMAT, i * inputLine, " ");
-                                });
-                                System.out.printf("%n");
-                            }
-                            
-                        } else {
-                            // 入力内容が数値以外の場合
-                            System.out.println("エラー！！数値以外が入力されました。");
-                            scanner.next();
+                                    });
+                            System.out.printf("%n");
                         }
+
+                    } else {
+                        // 入力内容が数値以外の場合
+                        System.out.println(NON_NUMERIC_ERROR);
+                        scanner.next();
                     }
-                    
-                    // 3が入力された場合
-                    if (line == 3) {
-                        // 切替
-                        if (displaySwitching) {
-                            displaySwitching = false;
-                        } else  {
-                            displaySwitching = true;
-                        }
-                        System.out.println("モードを切り替えました。");
-                    }
-                    
-                    
-                    // 9が入力された場合
-                    if (line == 9) {
-                        System.out.println("終了します。");
-                        break;
-                    }
-                    
-                } else {
-                    // 入力内容が数値以外の場合
-                    System.out.println("エラー！！数値以外が入力されました。");
-                    scanner.next();
                 }
+
+                // 3が入力された場合
+                if (line == 3) {
+                    // 切替
+                    if (displaySwitching) {
+                        displaySwitching = false;
+                    } else {
+                        displaySwitching = true;
+                    }
+                    System.out.println("モードを切り替えました。");
+                }
+
+                // 9が入力された場合
+                if (line == 9) {
+                    System.out.println("終了します。");
+                    break;
+                }
+
+            } else {
+                // 入力内容が数値以外の場合
+                System.out.println(NON_NUMERIC_ERROR);
+                scanner.next();
             }
-            scanner.close();
-            
+        }
+        scanner.close();
+
     }
 }
