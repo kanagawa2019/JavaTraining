54b157f83544ff9a0a6f5fd6c06ccbe0fe26fd07
developlocal <developlocalhm2019@gmail.com>
2021-05-01 16:17:08 +0900

    一旦上げる

---
+++ agatasan_java/src/agatasan_java/AccountHistory.java
@@ -0,0 +1,122 @@
+package agatasan_java;
+
+import java.util.Date;
+
+/**
+ * 口座履歴クラス
+ * 
+ * @author 菱田 美紀
+ * @version 1.0 2021/01/03 新規作成
+ */
+public class AccountHistory {
+
+    /** 口座番号 */
+    private String accountNumber;
+    /** 残高 */
+    private int balance;
+    /** 日付 */
+    private Date date;
+    /** 区分 */
+    private Bank classification;
+    /** 取引金額 */
+    private int transactionAmount;
+
+    /**
+     * コンストラクタ
+     * 
+     */
+    AccountHistory() {
+
+    }
+
+    /**
+     * 口座番号を取得
+     * 
+     * @return accountNumber
+     */
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * 口座番号を設定
+     * 
+     * @param accountNumber 口座番号をセットする
+     */
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    /**
+     * 残高を取得
+     * 
+     * @return balance
+     */
+    public int getBalance() {
+        return balance;
+    }
+
+    /**
+     * 残高を設定
+     * 
+     * @param balance 残高をセットする
+     */
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    /**
+     * 日付を取得
+     * 
+     * @return date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * 日付を設定
+     * 
+     * @param date 日付をセットする
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * 区分を取得
+     * 
+     * @return classification
+     */
+    public Bank getClassification() {
+        return classification;
+    }
+
+    /**
+     * 区分を設定
+     * 
+     * @param classification 区分をセットする
+     */
+    public void setClassification(Bank classification) {
+        this.classification = classification;
+    }
+
+    /**
+     * 取引金額を取得
+     * 
+     * @return transactionAmount
+     */
+    public int getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    /**
+     * 取引金額を設定
+     * 
+     * @param transactionAmount 取引金額をセットする
+     */
+    public void setTransactionAmount(int transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+}
